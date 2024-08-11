ab0d314be0bfe58856bd8e231131134fe2622bf2
huyan <34530704+Tig01er@users.noreply.github.com>
2024-08-11 21:33:03 +1200

    update config

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ export const config = {
   databaseURI:
     process.env.DATABASE_URI || process.env.MONGODB_URI || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'hydcabkcne-',
-  masterKey: process.env.MASTER_KEY || 'ehnycdkcba-', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
+  appId: 'hydcabkcne-',
+  masterKey: 'ehnycdkcba-', //Add your master key here. Keep it secret!
+  serverURL: 'http://localhost:1337/parse', // Don't forget to change to https if needed
   liveQuery: {
     classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
   },
